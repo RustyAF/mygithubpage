e7f2395d72a8d6f293655bafde9285cef58a37ae
Sebastian Medina <srmedinac@unal.edu.co>
2016-05-14 17:14:55 -0500

    Update temp-hum.py

--- temp-hum.py
+++ temp-hum.py
@@ -6,7 +6,7 @@ from pubnub import Pubnub
 import Adafruit_DHT as dht
 
 pubnub = Pubnub(publish_key='pub-c-21d3cb4c-7809-43f5-938e-2c6139510cc9', subscribe_key='sub-c-108945de-0ffa-11e6-a5b5-0619f8945a4f')
-channel = 'pi-house'
+channel = 'smart-home'
 
 def callback(message):
     print(message)
