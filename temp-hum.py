b70e79a1bacd4a7d3c23f8a95e889fdb841624ba
Sebastian Medina <srmedinac@unal.edu.co>
2016-05-14 17:14:38 -0500

    changed Sensor info and keys

--- temp-hum.py
+++ temp-hum.py
@@ -5,7 +5,7 @@ import sys
 from pubnub import Pubnub
 import Adafruit_DHT as dht
 
-pubnub = Pubnub(publish_key='demo', subscribe_key='demo')
+pubnub = Pubnub(publish_key='pub-c-21d3cb4c-7809-43f5-938e-2c6139510cc9', subscribe_key='sub-c-108945de-0ffa-11e6-a5b5-0619f8945a4f')
 channel = 'pi-house'
 
 def callback(message):
@@ -13,7 +13,7 @@ def callback(message):
 
 #published in this fashion to comply with Eon
 while True:
-    h,t = dht.read_retry(dht.DHT22, 4)
+    h,t = dht.read_retry(dht.DHT11, 4)
     temp={0:0.1f}.format(t)
     hum={1:0.1f}.format(h)
     message = {'temperature': temp, 'humidity': hum}
