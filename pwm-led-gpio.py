b6e1d10b97384a73f962282bd2718dd56b771776
Sebastian Medina <srmedinac@unal.edu.co>
2016-05-14 17:13:44 -0500

    changed keys

--- pwm-led-gpio.py
+++ pwm-led-gpio.py
@@ -31,9 +31,9 @@ fire.start(0)
 
 # PubNub
 
-pubnub = Pubnub(publish_key='demo', subscribe_key='demo')
+pubnub = Pubnub(publish_key='pub-c-21d3cb4c-7809-43f5-938e-2c6139510cc9', subscribe_key='sub-c-108945de-0ffa-11e6-a5b5-0619f8945a4f')
 
-channel = 'pi-house'
+channel = 'smart-home'
 
 def _callback(m, channel):
     print(m)
