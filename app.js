27512af4a8aa28596a2a41c110bf8e2085b505be
Sebastian Medina <srmedinac@unal.edu.co>
2016-05-15 21:42:33 -0500

    servo var

--- app.js
+++ app.js
@@ -11,6 +11,11 @@
 		publish_key: 'pub-c-21d3cb4c-7809-43f5-938e-2c6139510cc9',
 		subscribe_key: 'sub-c-108945de-0ffa-11e6-a5b5-0619f8945a4f'
 	}
+	var servo = {
+		channel: 'door',
+		publish_key: 'pub-c-21d3cb4c-7809-43f5-938e-2c6139510cc9',
+		subscribe_key: 'sub-c-108945de-0ffa-11e6-a5b5-0619f8945a4f'
+	}
 
 	var pubnub = PUBNUB(settings);
 
@@ -49,6 +54,12 @@
 	function publishUpdate(data) {
 		pubnub.publish({
 			channel: lights.channel, 
+			message: data
+		});
+	}
+	function updateDoor(data) {
+		pubnub.publish({
+			channel: servo.channel, 
 			message: data
 		});
 	}
