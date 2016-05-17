5c8d4569b46888e668e651b2d75399df2bf7751d
Sebastian Medina <srmedinac@unal.edu.co>
2016-05-17 15:28:30 -0500

    uncomment

--- start_home.sh
+++ start_home.sh
@@ -2,6 +2,6 @@
 #init smart home
 cd /
 cd /home/pi/smarthome/
-#sudo python pwm-led-gpio.py &
-#sudo python sensor.py &
-#sudo python door.py &
+sudo python pwm-led-gpio.py &
+sudo python sensor.py &
+sudo python door.py &
